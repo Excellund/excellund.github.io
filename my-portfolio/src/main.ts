216361febcc38e3ef4c646989008a730c3ce01eb
Excellund <mikela@outlook.dk>
2023-10-16 15:37:46 +0200

    three js

---
+++ my-portfolio/src/main.ts
@@ -0,0 +1,63 @@
+import * as THREE from 'https://unpkg.com/three@0.157.0/build/three.module.js';
+import CameraControl from './CameraControl';
+
+// Setup THREE
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+
+// Setup Renderer
+renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
+
+// Add Renderer to the body
+document.body.appendChild(renderer.domElement);
+
+// Geometry
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+
+// Material
+const greenMaterial = new THREE.MeshPhongMaterial({color: 0x00FF00});
+const greyMaterial = new THREE.MeshPhongMaterial({color: 0x5A5A5A});
+
+// Mesh
+const boxMesh = new THREE.Mesh(boxGeometry, greenMaterial);
+const planeMesh = new THREE.Mesh(planeGeometry, greyMaterial);
+
+// Light
+const light = new THREE.DirectionalLight(0xffffff, 1);
+const light2 = new THREE.DirectionalLight(0xffffff, 0.5);
+const light3 = new THREE.DirectionalLight(0xffffff, 0.25);
+
+// Position the Camera
+camera.position.z = 5;
+camera.position.y = 3;
+camera.lookAt(new THREE.Vector3(0,0,0));
+
+// Position the Plane
+planeMesh.rotation.x = -Math.PI / 2;
+
+// Position the Light
+light.position.set(0, 1, 0);
+light2.position.set(1, 1, 1);
+light3.position.set(-1, 1, -1);
+
+// Scene Additions
+scene.add(boxMesh)
+scene.add(planeMesh);
+scene.add(light);
+scene.add(light2);
+scene.add(light3);
+
+// Animation
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
+
+// Gain Camera Control
+const cameraControl = new CameraControl(renderer, camera, () => {
+  requestAnimationFrame(() => renderer.render(scene, camera))
+})
